c7f2a05b4ce72b011afad1ae568623c1828b916f
Micael Jerry <hei.jerry.2@gmail.com>
2022-09-08 07:07:51 +0300

    fix: link navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import {Link, useNavigate} from "react-router-dom";
+import {Link} from "react-router-dom";
 
-const Navbar:React.FC<{}> = (props) => {
-    const navigate = useNavigate();
+const Navbar: React.FC<{}> = (props) => {
     return (
         <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
             <div className="container-fluid">
@@ -15,29 +14,21 @@ const Navbar:React.FC<{}> = (props) => {
                 <div className="collapse navbar-collapse" id="navbarCollapse">
                     <ul className="navbar-nav me-auto mb-2 mb-md-0">
                         <li className="nav-item">
-                            <a className="nav-link active" onClick={() => {
-                                navigate("/")
-                            }}>Home</a>
+                            <Link to={"/"} className="nav-link">Home</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" onClick={() => {
-                                navigate("/fee")
-                            }}>Fee</a>
+                            <Link to={"/fee"} className="nav-link">Fee</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" onClick={() => {
-                                navigate("/transaction")
-                            }}>Transaction</a>
+                            <Link to={"/transaction"} className="nav-link">Transaction</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" onClick={() => {
-                                navigate("/student")
-                            }}>Student</a>
+                            <Link to={"/student"} className="nav-link">Student</Link>
                         </li>
                     </ul>
                     <form className="d-flex">
                         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                            <button className="btn btn-outline-success" type="submit">Search</button>
+                        <button className="btn btn-outline-success" type="submit">Search</button>
                     </form>
                 </div>
             </div>

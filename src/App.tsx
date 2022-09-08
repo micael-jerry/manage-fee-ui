a83aafb1c930f235ee429720a9b8f9e0598472df
Micael Jerry <hei.jerry.2@gmail.com>
2022-09-08 07:24:31 +0300

    fix: add NotFound page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Home from "./components/pages/Home";
 import Student from "./components/pages/Student";
 import Navbar from "./components/layout/Navbar";
 import {Route, Routes} from "react-router-dom";
+import NotFound from "./components/pages/NotFound";
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
             <Routes>
                 <Route path={"/"} element={<Home/>}></Route>
                 <Route path={"/student"} element={<Student/>}></Route>
+                <Route path={"*"} element={<NotFound/>}></Route>
             </Routes>
             <Navbar/>
         </>
---
+++ src/components/pages/NotFound.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFound:React.FC<{}> = (props) => {
+    return (
+        <div>
+            <h1>Not Found</h1>
+        </div>
+    )
+}
+
+export default NotFound;
